711ddd69b0720b962deb1d53874154cf949a4e63
Stephen Garriga <steve_garriga@yahoo.com>
2023-10-03 11:54:49 -0400

    Add check for running WSPR Tx

--- check_wifi.sh
+++ check_wifi.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #
-IP=`ip route | grep default | cut -d " " -f 3`
-ping -c4 ${IP}  > /dev/null
-if [ $? != 0 ] 
+wspr=`pgrep wspr`
+if [ -z "$wspr" ]
 then
-  echo "No network connection, restarting wlan0"
-  /sbin/ifdown 'wlan0'
-  sleep 5
-  /sbin/ifup --force 'wlan0'
+    IP=`ip route | grep default | cut -d " " -f 3`
+    ping -c4 ${IP}  > /dev/null
+    if [ $? != 0 ] 
+    then
+      echo "No network connection, restarting wlan0"
+      /sbin/ifdown 'wlan0'
+      sleep 5
+      /sbin/ifup --force 'wlan0'
+    fi
+else
+    echo WSPR running - deferring WIFI check
 fi
